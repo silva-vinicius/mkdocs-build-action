0d21b61fe6bf95d682656e1e1a2ab7f6e7ecfd5f
Vinicius Silva <vinicius.silva@bigdata.com.br>
2023-01-13 14:37:41 -0300

    removes docker cp and exec from script

--- setup-docker.sh
+++ setup-docker.sh
@@ -11,11 +11,4 @@ container_id=$(docker run -d -it -v $(pwd)/public:/public python:$python_version
 
 touch super_file.txt
 
-docker cp build-docs.sh $container_id:/build-docs.sh
-
-docker exec $container_id /bin/bash -c 'chmod +x /build-docs.sh; /build-docs.sh'
-
-echo "built_dir=public" >> $GITHUB_OUTPUT
-
-# The folder my_folder is now accessible inside the container
-ls
+echo "container_id=$container_id" >> $GITHUB_OUTPUT
